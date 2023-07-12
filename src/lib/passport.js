e184fe5428eb11496213727fa0150deefee61ea6
yzmaya <nestor.tarinda@gmail.com>
2023-07-12 15:04:42 -0600

    subiendo el proyecto desde cero
    git branch -M main
    git remote add origin https://github.com/yzmaya/copazetest.git
    git push -u origin main

---
+++ src/lib/passport.js
@@ -0,0 +1,71 @@
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+
+const pool = require('../database');
+const helpers = require('./helpers');
+
+passport.use('local.signin', new LocalStrategy({
+  usernameField: 'username',
+  passwordField: 'password',
+  passReqToCallback: true
+}, async (req, username, password, done) => {
+  const rows = await pool.query('SELECT * FROM users WHERE username = ?', [username]);
+  if (rows.length > 0) {
+    const user = rows[0];
+    const validPassword = await helpers.matchPassword(password, user.password)
+    if (validPassword) {
+      done(null, user, req.flash('success', 'Welcome ' + user.username));
+    } else {
+      done(null, false, req.flash('message', 'Incorrect Password'));
+    }
+  } else {
+    return done(null, false, req.flash('message', 'The Username does not exists.'));
+  }
+}));
+
+passport.use('local.signup', new LocalStrategy({
+  usernameField: 'username',
+  passwordField: 'password',
+  passReqToCallback: true
+}, async (req, username, password, done) => {
+
+  const { fullname } = req.body;
+  const { apellido1 } = req.body;
+  const { apellido2 } = req.body;
+  const { cct } = req.body;
+  const { comision1 } = req.body;
+  const { comision2 } = req.body;
+  const { comision3 } = req.body;
+  const { comision4 } = req.body;
+  const { comision5 } = req.body;
+
+
+  let newUser = {
+    fullname,
+    apellido1,
+    apellido2,
+    username,
+    password,
+    cct,
+    comision1,
+    comision2,
+    comision3,
+    comision4,
+    comision5
+  }; 
+  newUser.password = await helpers.encryptPassword(password);
+  // Saving in the Database
+  const result = await pool.query('INSERT INTO users SET ? ', newUser);
+  newUser.id = result.insertId;
+  return done(null, newUser);
+}));
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  const rows = await pool.query('SELECT * FROM users WHERE id = ?', [id]);
+  done(null, rows[0]);
+});
+
---
+++ src/routes/links.js
@@ -0,0 +1,101 @@
+const express = require('express'),
+      multer = require("multer");
+const router = express.Router();
+const path = require('path');
+const app = express();
+const pool = require('../database');
+const { isLoggedIn } = require('../lib/auth');
+
+const upload = multer({storage:multer.memoryStorage()});
+
+
+//multer storagelocal
+//app.use(express.static(path.join(__dirname, 'public')));
+
+const storage = multer.diskStorage({
+    destination: path.join(__dirname, '../public/uploads'),
+    filename: (req, file, cb) => {
+        cb(null, file.originalname);
+    }
+})
+
+router.use(multer({
+    storage,
+    dest: path.join(__dirname, '../public/uploads'),
+    limits: {fileSize: 1000000},//acepta 1 mb
+    fileFilter: (req, file, cb) => {
+        const filetypes = /jpg|pdf|png/;
+        const mimetype = filetypes.test(file.mimetype);
+        const extname = filetypes.test(path.extname(file.originalname));
+        if(mimetype && extname){
+            return cb(null,true);
+        }
+        cb("error: la extension de tu archivo no es correcta")
+    }
+
+}).single('archivo1'))
+
+router.get('/add', (req, res) => {
+    res.render('links/add');
+});
+
+
+
+
+router.post('/add', async (req, res) => {
+   
+    const archivo1 = req.file.originalname;
+    const { tipo, comision, nombre, apellido1, apellido2, cargo, dependencia} = req.body;
+    const newLink = {
+        tipo,
+        comision,
+        nombre,
+        apellido1,
+        apellido2,
+        cargo,
+        dependencia,
+        archivo1,
+        user_id: req.user.id
+    };
+    await pool.query('INSERT INTO links set ?', [newLink]);
+    req.flash('success', 'Link Saved Successfully');
+    res.redirect('/links');
+});
+
+router.get('/', isLoggedIn, async (req, res) => {
+    const links = await pool.query('SELECT * FROM links WHERE user_id = ?', [req.user.id]);
+    res.render('links/list', { links });
+});
+
+router.get('/delete/:id', async (req, res) => {
+    const { id } = req.params;
+    await pool.query('DELETE FROM links WHERE ID = ?', [id]);
+    req.flash('success', 'Link Removed Successfully');
+    res.redirect('/links');
+});
+
+router.get('/edit/:id', async (req, res) => {
+    const { id } = req.params;
+    const links = await pool.query('SELECT * FROM links WHERE id = ?', [id]);
+    console.log(links);
+    res.render('links/edit', {link: links[0]});
+});
+
+router.post('/edit/:id', async (req, res) => {
+    const { id } = req.params;
+    const { tipo, comision, nombre, apellido1, apellido2, cargo, dependencia} = req.body; 
+    const newLink = {
+        tipo,
+        comision,
+        nombre,
+        apellido1,
+        apellido2,
+        cargo,
+        dependencia
+    };
+    await pool.query('UPDATE links set ? WHERE id = ?', [newLink, id]);
+    req.flash('success', 'Link Updated Successfully');
+    res.redirect('/links');
+});
+
+module.exports = router;
