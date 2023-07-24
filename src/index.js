ad0b6444aa1de081371093364e1cfefa1c51ca5f
karenJasso <138605724+karenJasso@users.noreply.github.com>
2023-07-24 12:14:31 -0600

    change port

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ const app = express();
 require('./lib/passport');
 
 // Settings
-app.set('port', process.env.PORT || 4000);
+app.set('port', process.env.PORT || 80);
 app.set('views', path.join(__dirname, 'views'));
 app.engine('.hbs', exphbs({
   defaultLayout: 'main',
