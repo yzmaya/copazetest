5c959fa666e2daa2edf8a0bd672af32b4c8771f4
yzmaya <nestor.tarinda@gmail.com>
2023-08-05 00:37:40 -0600

    s

--- src/lib/mailer.js
+++ src/lib/mailer.js
@@ -5,13 +5,8 @@ const transporter = nodemailer.createTransport({
     port: 587,
     secure: false,
     auth: {
-<<<<<<< HEAD
         user: "tucorreo",
         pass: "tucontrasena",
-=======
-        user: "karen.jasso@sems.gob.mx",
-        pass: "kar3n29**",
->>>>>>> c655b2f49e9df8a3c6e99d0257711237a7434de4
     }
 });
 
